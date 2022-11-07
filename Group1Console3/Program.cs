79b6577532fc2e6b6226a34ceeae220fef7a409a
Nusret Bilallaj <netktit@gmail.com>
2022-11-07 18:12:45 +0100

    Add project files.

---
+++ Group1Console3/Program.cs
@@ -0,0 +1,26 @@
+﻿using Group1Console3;
+
+Console.WriteLine("Mire se vini ne Kolegjin AAB!");
+////tri kategori te variableave
+//int numri=1;
+//string emri="Leron Berisha";
+//bool kushti=true;
+//kushti=true;
+////Console.WriteLine($"{emri} eshte numer {numri}");
+////Console.WriteLine($"A eshte e vertete a jo? {kushti}");
+
+//void ParaqitProfesorin(string prof,int numrip,bool kushtip)
+//{
+//    Console.WriteLine($"{prof} eshte numer {numrip}");
+//    Console.WriteLine($"A eshte e vertete a jo? {kushtip}");
+//}
+//ParaqitProfesorin("Leron Berisha",1,true);
+
+int numri2 = 100;
+Studenti studenti = new Studenti("Edi","Morina",21);
+studenti.Pershendete();
+
+
+
+
+
---
+++ Group1Console3/Studenti.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1Console3
+{
+    internal class Studenti
+    {
+        public Studenti(string emri,string mbiemri,int mosha)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+            Mosha = mosha;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+        public int Mosha { get; set; }
+
+        public void Pershendete()
+        {
+            Console.WriteLine($"Tungjatjeta {Emri},{Mbiemri}");
+        }
+
+    }
+}
