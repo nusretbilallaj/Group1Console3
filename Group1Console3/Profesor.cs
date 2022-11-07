2a62db62d239a21816b9e6521c5fe8ef4546e651
Nusret Bilallaj <netktit@gmail.com>
2022-11-07 18:42:02 +0100

    Eshte sthuar metoda

---
+++ Group1Console3/Profesor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group1Console3
+{
+    internal class Profesor
+    {
+        public Profesor(string emri, string mbiemri)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+        }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+        public void Pershendete()
+        {
+            Console.WriteLine($"Tungjatjeta profesor {Emri},{Mbiemri}");
+        }
+    }
+}
